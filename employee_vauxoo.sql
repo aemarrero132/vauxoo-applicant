e6696e455acb9aa2d46fb308ce7a2cc1bd2c4bc8
aemarrero132 <aemarrero132@gmail.com>
2016-10-04 18:55:14 -0400

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,12 +4,26 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+  id int not null,
+  first_name varchar (30) not null,
+  last_name varchar (30) not null,
+  dep_id int not null,
+  PRIMARY KEY (id),
+  FOREIGN KEY (dep_id) REFERENCES employee_departament (id)
 );
 
 CREATE TABLE employee_department (
+  id int not null,
+  name varchar (30) not null,
+  description (30) not null,
+  primary key (id)
 );
 
 CREATE TABLE employee_hobby (
+  id int not null,
+  name varchar (30) not null,
+  description (30) not null,
+  primary key (id)
 );
 
 -- ...
