d503c6b1c8e1861c0862a11fe6c79504267319a5
aemarrero132 <aemarrero132@gmail.com>
2016-10-04 22:33:57 -0400

    Update primes.py
    Metodo is_prime.

--- primes.py
+++ primes.py
@@ -4,13 +4,21 @@ Your module documentation here
 
 
 class PrimeClass(object):
-    """
-    Your class documentation here
-    """
+    '''
+    Clase PrimeClass
+    '''
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Método is_prime que recibe un numero entero y devuelve True (si es numero primo) o False (numero compuesto) 
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        veces = 0
+        for i in range(1, num_int+1):
+            if num_int % i == 0:
+                print(i)
+                veces = veces + 1
+        if veces > 2:    
+            return False
+        else:
+            return True
