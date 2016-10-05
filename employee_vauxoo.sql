0d4334781247c10f9268137b209afc027820f86c
aemarrero132 <aemarrero132@gmail.com>
2016-10-05 19:13:47 -0400

    sql1

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,40 +3,38 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
+CREATE TABLE employee_department (
+  id int not null,
+  name varchar (30) not null,
+  description varchar (30) not null,
+  CONSTRAINT employee_department_pk PRIMARY KEY (id)
+);
+
 CREATE TABLE employee (
   id int not null,
   first_name varchar (30) not null,
   last_name varchar (30) not null,
-  dep_id int not null,
-  PRIMARY KEY (id),
-  FOREIGN KEY (dep_id) REFERENCES employee_departament (id)
+  employee_department_id int not null,
+  CONSTRAINT employee_pk PRIMARY KEY (id),
+  CONSTRAINT employee_employee_department_fk FOREIGN KEY (employee_department_id) REFERENCES employee_department(id)
 );
 
-CREATE TABLE employee_department (
-  id int not null,
-  name varchar (30) not null,
-  description (30) not null,
-  primary key (id)
-);
-
-CREATE TABLE employee_hobby (
-  id int not null,
-  name varchar (30) not null,
-  description (30) not null,
-  primary key (id)
-);
-
+INSERT INTO employee_department VALUES (1, 'rrhh', 'recursos humanos');
+INSERT INTO employee_department VALUES (2, 'sistemas', 'computacion');
+INSERT INTO employee_department VALUES (3, 'ventas', 'ventas');
+INSERT INTO employee_department VALUES (4, 'nomina', 'nomina');
+INSERT INTO employee_department VALUES (5, 'contabilidad', 'contable');
+INSERT INTO employee_department VALUES (6, 'compras', 'compras');
 INSERT INTO employee VALUES (1, 'andres', 'marrero', 1);
 INSERT INTO employee VALUES (2, 'jose', 'gomez', 3);
 INSERT INTO employee VALUES (3, 'daniela', 'ocanto', 2);
 INSERT INTO employee VALUES (4, 'jose', 'perez', 5);
 
-INSERT INTO employee VALUES (1, 'rrhh', 'recursos humanos');
-INSERT INTO employee VALUES (2, 'sistemas', 'computacion');
-INSERT INTO employee VALUES (3, 'ventas', 'ventas');
-INSERT INTO employee VALUES (4, 'nomina', 'nomina');
-INSERT INTO employee VALUES (5, 'contabilidad', 'contable');
-INSERT INTO employee VALUES (6, 'compras', 'compras');
+
+
+
+
+
 
 
 -- ...
