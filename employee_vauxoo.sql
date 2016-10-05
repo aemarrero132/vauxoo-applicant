16fc4661124c0c6648c619765c22881d3f3de6c1
aemarrero132 <aemarrero132@gmail.com>
2016-10-05 19:37:19 -0400

    sql2

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -30,11 +30,31 @@ INSERT INTO employee VALUES (2, 'jose', 'gomez', 3);
 INSERT INTO employee VALUES (3, 'daniela', 'ocanto', 2);
 INSERT INTO employee VALUES (4, 'jose', 'perez', 5);
 
+CREATE TABLE employee_hobby (
+  id int not null,
+  name varchar (30) not null,
+  description varchar (30) not null,
+  CONSTRAINT employee_hobby_pk PRIMARY KEY (id)
+);
 
+INSERT INTO employee_hobby VALUES (1, 'dance', 'salsa casino');
+INSERT INTO employee_hobby VALUES (2, 'games', 'videojuegos');
+INSERT INTO employee_hobby VALUES (3, 'movies', 'cine');
 
+CREATE TABLE relacion_employee_hobby (
+  id_employee int not null,
+  id_employee_hobby int not null,
+  CONSTRAINT relacion_employee_hobby_pk PRIMARY KEY (id_employee, id_employee_hobby),
+  CONSTRAINT relacion_employee_hobby_fk1 FOREIGN KEY (id_employee) REFERENCES employee(id),
+  CONSTRAINT relacion_employee_hobby_fk2 FOREIGN KEY (id_employee_hobby) REFERENCES employee_hobby(id)
+);
 
-
-
-
-
--- ...
+INSERT INTO relacion_employee_hobby VALUES (1, 1);
+INSERT INTO relacion_employee_hobby VALUES (1, 2);
+INSERT INTO relacion_employee_hobby VALUES (1, 3);
+INSERT INTO relacion_employee_hobby VALUES (2, 2);
+INSERT INTO relacion_employee_hobby VALUES (2, 3);
+INSERT INTO relacion_employee_hobby VALUES (3, 1);
+INSERT INTO relacion_employee_hobby VALUES (3, 3);
+INSERT INTO relacion_employee_hobby VALUES (4, 1);
+INSERT INTO relacion_employee_hobby VALUES (4, 2);
