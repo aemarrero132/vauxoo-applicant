573ab380e8f1b1d21efec8d7ee04c51f77e045bf
aemarrero132 <aemarrero132@gmail.com>
2016-10-05 22:17:40 -0400

    ultimo commit

--- primes.py
+++ primes.py
@@ -1,22 +1,21 @@
 """
-Your module documentation here
+Clase PrimeClass, con metodo de verificacion de numeros primos.
+Vauxoo Aplicant
 """
-
 
 class PrimeClass(object):
     '''
     Clase PrimeClass
     '''
-
     def is_prime(self, num_int):
         """
         Método is_prime que recibe un numero entero y devuelve True (si es numero primo) o False (numero compuesto) 
         """
         # your primes code here
-        veces = 0
+        veces=0
         for i in range(2, num_int):
             if num_int % i == 0
-                veces = veces + 1
+                veces=veces+1
         if veces > 1:    
             return False
         else:
