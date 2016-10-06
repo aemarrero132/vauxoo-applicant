bfd756d5ed271d36a578e3678ee308064d994cc4
aemarrero132 <aemarrero132@gmail.com>
2016-10-06 07:09:27 -0400

    Optimizacion del codigo

--- primes.py
+++ primes.py
@@ -12,11 +12,10 @@ class PrimeClass(object):
         Método is_prime que recibe un numero entero y devuelve True (si es numero primo) o False (numero compuesto) 
         """
         # your primes code here
-        veces=0
+        veces=True
         for i in range(2, num_int):
             if num_int % i == 0:
-                veces=veces+1
-        if veces > 1:    
-            return False
-        else:
-            return True
+                veces=False
+                break
+        
+        return veces
